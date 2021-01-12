d657d8d5ade40fd31d86449ec53f0c96a81f9be0
Chadwin <chadwinjdeysel@gmail.com>
2021-01-12 20:06:53 +0200

    added savings goals

---
+++ FinanceTracker/Controllers/SavingsGoalController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FinanceTracker.Data;
+using FinanceTracker.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceTracker.Controllers
+{
+    public class SavingsGoalController : Controller
+    {
+        private readonly IFinanceRepo _repo;
+
+        public SavingsGoalController(IFinanceRepo repo)
+        {
+            this._repo = repo;
+        }
+
+        [Route("~/Savings")]
+        public async Task<IActionResult> Goals()
+        {
+            var goals = await _repo.GetList<SavingsGoal>();
+
+            return View(goals);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(SavingsGoal model)
+        {
+            if (ModelState.IsValid)
+            {
+                _repo.Add<SavingsGoal>(model);
+
+                if (await _repo.SaveAll())
+                    return RedirectToAction("Goals");
+
+                throw new Exception("Something went wrong, trying to add new Savings Goal");
+            }
+
+            return View();
+        }
+
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var goal = await _repo.Find<SavingsGoal>(id);
+
+            if (goal == null)
+                return NotFound();
+
+            return View(goal);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(SavingsGoal goal)
+        {
+            if (ModelState.IsValid)
+            {
+                _repo.Edit<SavingsGoal>(goal);
+
+                if (await _repo.SaveAll())
+                    return RedirectToAction("Goals");
+            }
+
+            return View();
+        }
+
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var goal = await _repo.Find<SavingsGoal>(id);
+
+            if (goal == null)
+                return NotFound();
+
+            _repo.Delete<SavingsGoal>(goal);
+
+            if(await _repo.SaveAll())
+                return RedirectToAction("Goals");
+
+            return RedirectToAction("Goals");
+        }
+    }
+}
--- FinanceTracker/Models/SavingsGoal.cs
+++ FinanceTracker/Models/SavingsGoal.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace FinanceTracker.Models
     public class SavingsGoal
     {
         public Guid Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public string Description { get; set; }
         public bool HasDeadline { get; set; }
