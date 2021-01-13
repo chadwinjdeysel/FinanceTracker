7d6ce94103eefd4e104e248528f339a6c90bea96
Chadwin <chadwinjdeysel@gmail.com>
2021-01-13 21:00:40 +0200

    updated forms for expenses and categories. updated the expenses tables design

--- FinanceTracker/Models/Category.cs
+++ FinanceTracker/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,7 +10,9 @@ namespace FinanceTracker.Models
     {
         public Guid Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
+        [Display(Name = "Color")]
         public string ColorCode { get; set; }
         public string Description { get; set; }
 
